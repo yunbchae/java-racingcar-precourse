430e062f68a788ee841ab7d35aebebc5ca2449e7
채윤병 <ybchae0801@naver.com>
2022-04-24 05:11:40 +0900

    feat: 자동차 이동 기능 구현
    - 이동 조건을 충족하는 경우, 자동차는 전진한다.

--- src/main/java/racingcar/domain/RacingCar.java
+++ src/main/java/racingcar/domain/RacingCar.java
@@ -5,8 +5,25 @@ public class RacingCar {
     private final RacingCarName name;
     private final RacingCarPosition position;
 
+    public RacingCar(final RacingCarName name, final RacingCarPosition position) {
+        validateNonNull(name, position);
+        this.name = name;
+        this.position = position;
+    }
+
     public RacingCar(final String name) {
-        this.name = new RacingCarName(name);
-        this.position = RacingCarPosition.startingLine();
+        this(new RacingCarName(name), RacingCarPosition.startingLine());
+    }
+
+    private void validateNonNull(final RacingCarName name, final RacingCarPosition position) {
+        if (name == null || position == null) {
+            throw new IllegalArgumentException(String.format("입력 값이 비어있습니다. name: %s, position: %s", name, position));
+        }
+    }
+
+    public void move(final MoveCondition moveCondition) {
+        if (moveCondition.isSatisfied()) {
+            position.move();
+        }
     }
 }
---
+++ src/test/java/racingcar/domain/RacingCarTest.java
@@ -0,0 +1,69 @@
+package racingcar.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.*;
+
+class RacingCarTest {
+
+    @DisplayName("RacingCar 생성 테스트")
+    @Nested
+    class CreateTest {
+
+        @DisplayName("RacingCarName과 RacingCarPosition으로 RacingCar를 생성할 수 있다.")
+        @Test
+        void createSuccessTest() {
+            assertThatNoException()
+                    .isThrownBy(() -> new RacingCar(new RacingCarName("aaa"), RacingCarPosition.startingLine()));
+        }
+
+        @ParameterizedTest(name = "RacingCarName 또는 RacingCarPosition이 null인 경우, IllegalArgumentException 예외가 발생한다.")
+        @MethodSource("racingcar.domain.RacingCarTest#provideCreateFailTestArguments")
+        void createFailTest(final RacingCarName name, final RacingCarPosition position) {
+            assertThatIllegalArgumentException()
+                    .isThrownBy(() -> new RacingCar(name, position))
+                    .withMessageContaining(String.valueOf(name), position);
+        }
+    }
+
+    @DisplayName("RacingCar 전진 테스트")
+    @Nested
+    class MoveTest {
+
+        @ParameterizedTest(name = "이동 조건이 충족하는 경우, 자동차는 전진한다.")
+        @MethodSource("racingcar.domain.RacingCarTest#provideMoveTestArguments")
+        void moveTest(final MoveCondition moveCondition,
+                      final RacingCarPosition actualPosition,
+                      final RacingCarPosition expectedPosition
+        ) {
+            final RacingCar racingCar = new RacingCar(new RacingCarName("aaa"), actualPosition);
+
+            racingCar.move(moveCondition);
+
+            assertThat(actualPosition)
+                    .isEqualTo(expectedPosition);
+        }
+    }
+
+    private static Stream<Arguments> provideCreateFailTestArguments() {
+        return Stream.of(
+                Arguments.of(null, RacingCarPosition.startingLine()),
+                Arguments.of(new RacingCarName("aaa"), null),
+                Arguments.of(null, null)
+        );
+    }
+
+    private static Stream<Arguments> provideMoveTestArguments() {
+        return Stream.of(
+                Arguments.of((MoveCondition) () -> true, new RacingCarPosition(0), new RacingCarPosition(1)),
+                Arguments.of((MoveCondition) () -> false, new RacingCarPosition(0), new RacingCarPosition(0))
+        );
+    }
+}
