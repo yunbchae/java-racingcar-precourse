79e2dab6d41271e46daac07d17aa40010bbd7928
채윤병 <ybchae0801@naver.com>
2022-04-24 18:59:45 +0900

    refactor: ListUtils 클래스 구현
    - 일반적인 List 기능을 ListUtils 클래스를 통해 구현한다.

--- src/main/java/racingcar/domain/RacingCars.java
+++ src/main/java/racingcar/domain/RacingCars.java
@@ -2,9 +2,11 @@ package racingcar.domain;
 
 import racingcar.util.Assert;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+
+import static racingcar.util.ListUtils.filter;
+import static racingcar.util.ListUtils.map;
 
 public class RacingCars {
 
@@ -18,26 +20,13 @@ public class RacingCars {
     public static RacingCars of(final List<String> carNameList) {
         Assert.notEmpty(carNameList, String.format("입력 값이 비어있습니다. input: %s", carNameList));
         Assert.notDuplicated(carNameList, String.format("중복된 이름이 존재합니다. input: %s", carNameList));
-
-        final List<RacingCar> racingCarList = new ArrayList<>();
-        for (String carName : carNameList) {
-            racingCarList.add(new RacingCar(carName));
-        }
-        return new RacingCars(racingCarList);
+        return new RacingCars(map(carNameList, RacingCar::new));
     }
 
     public List<RacingCar> getWinners() {
         final RacingCar winner = Collections.max(racingCarList);
-        final List<RacingCar> winners = new ArrayList<>();
-        for (RacingCar racingCar : racingCarList) {
-            judgeWinner(winner, winners, racingCar);
-        }
+        final List<RacingCar> winners = filter(racingCarList, racingCar -> racingCar.isAtSamePosition(winner));
         return Collections.unmodifiableList(winners);
     }
 
-    private void judgeWinner(final RacingCar winner, final List<RacingCar> winners, final RacingCar candidate) {
-        if (candidate.isAtSamePosition(winner)) {
-            winners.add(candidate);
-        }
-    }
 }
